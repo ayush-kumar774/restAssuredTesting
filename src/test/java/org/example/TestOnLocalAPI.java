eef291a452a24b4e38f25ca34aa83427b07b0f4c
ayush-kumar774 <ayushkumar1234@protonmail.com>
2021-08-18 13:12:50 +0530

    Added Test on Local API

--- src/test/java/org/example/TestOnLocalAPI.java
+++ src/test/java/org/example/TestOnLocalAPI.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import io.restassured.http.ContentType;
+import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 import  static io.restassured.RestAssured.*;
 
@@ -11,7 +13,73 @@ public class TestOnLocalAPI {
         given().
                 get("/users").
                     then().
-                statusCode(200).
-                log().all();
+                statusCode(200);
+//                .
+//                log().all();
+    }
+
+    @Test
+    public void post()
+    {
+        JSONObject request = new JSONObject() ;
+        request.put("firstName", "Gautam");
+        request.put("lastName", "Rana");
+        request.put("subjectID", 2);
+
+        baseURI = "http://localhost:3000" ;
+        given().
+                contentType(ContentType.JSON).
+                accept(ContentType.JSON).
+                body(request.toJSONString()).
+                when().
+                post("/users").
+                then().
+                statusCode(201).log().all();
+    }
+
+    @Test
+    public void put()
+    {
+        JSONObject request = new JSONObject();
+        request.put("firstName" , "Jack");
+        request.put("lastName", "Pitt");
+        request.put("subjectID" , 1);
+
+        baseURI = "http://localhost:3000";
+
+        given().
+                contentType(ContentType.JSON).
+                accept(ContentType.JSON).
+                body(request.toJSONString()).
+                when().
+                    put("/users/4").
+                then().
+                    statusCode(200).log().all();
+    }
+
+    @Test
+    public void patch()
+    {
+        JSONObject request = new JSONObject();
+
+        request.put("lastName" , "Torreto");
+
+        baseURI = "http://localhost:3000";
+
+        given().
+                contentType(ContentType.JSON).
+                accept(ContentType.JSON).
+                body(request.toJSONString()).
+                when().
+                    patch("/users/4").
+                then().
+                statusCode(200).log().all();
+    }
+    @Test
+    public void delete()
+    {
+        // json-server --watch db.json to start json server
+        baseURI = "http://localhost:3000" ;
+        when().delete("/users/2").then().statusCode(200).log().all();
     }
 }
