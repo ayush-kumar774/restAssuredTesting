7955d70c2afa0ba685c37e69d1db667e69ba21ef
ayush-kumar774 <ayushkumar1234@protonmail.com>
2021-08-18 11:59:29 +0530

    Added Test on Local API

--- src/test/java/org/example/TestOnLocalAPI.java
+++ src/test/java/org/example/TestOnLocalAPI.java
@@ -1,4 +1,17 @@
 package org.example;
 
+import org.testng.annotations.Test;
+import  static io.restassured.RestAssured.*;
+
 public class TestOnLocalAPI {
+    @Test
+    public void get()
+    {
+        baseURI = "http://localhost:3000" ;
+        given().
+                get("/users").
+                    then().
+                statusCode(200).
+                log().all();
+    }
 }
